08e51c3b7b415cb00167c2b5b47e48a7f5fb1fc2
darth77 <93335986+darth77@users.noreply.github.com>
2024-06-13 09:41:15 -0400

    Delete Office16Activaton.bat

--- Office16Activaton.bat
+++
@@ -1,9 +0,0 @@
-cd "C:\Program Files\Microsoft Office\Office16"
-
-cscript ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
-
-cscript ospp.vbs /sethst:ddccdc1.dccc.local
-
-cscript ospp.vbs /setprt:1688 
-
-cscript ospp.vbs /act
