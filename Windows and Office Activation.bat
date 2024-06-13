6dafc08ade0eb541e1e02ae25543339b3b846177
darth77 <93335986+darth77@users.noreply.github.com>
2024-06-13 09:41:25 -0400

    Delete Windows and Office Activation.bat

--- Windows and Office Activation.bat
+++
@@ -1,6 +0,0 @@
-slmgr.vbs /skms ddccdc1.dccc.local:1688
-slmgr.vbs /ato
-cd c:\program files\microsoft office\office16
-cscript ospp.vbs /sethst:ddccdc1.dccc.local
-cscript ospp.vbs /setprt:1688
-cscript ospp.vbs /act
